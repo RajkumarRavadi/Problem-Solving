655f429deb58bf407cde0e3416f28ba802742b73
RajkumarRavadi <rajk142567@gmail.com>
2023-08-01 18:04:59 +0530

    swaping programs added

---
+++ python/Simple Programs/swap_two_varible_values.py
@@ -0,0 +1,5 @@
+a=5
+b=10
+print("Before Swaping {} and {} ".format(a,b))
+a,b=b,a
+print("After Swaping {} and {} ".format(a,b))
---
+++ python/Simple Programs/swap_two_varible_values_using_temp.py
@@ -0,0 +1,8 @@
+a=5
+b=10
+temp=0
+print("Before swaping {} and {}".format(a,b))
+temp=a
+a=b
+b=temp
+print("After swaping {} and {}".format(a,b))

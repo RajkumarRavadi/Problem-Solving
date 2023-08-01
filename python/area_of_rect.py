3004d2eb09bd0b693f73ba042d469a034944dc66
RajkumarRavadi <rajk142567@gmail.com>
2023-08-01 10:44:46 +0530

    area of rectangle problem added

---
+++ python/area_of_rect.py
@@ -0,0 +1,12 @@
+"""
+    To find the area of a rectangle we need to know 
+    height and width of the rectagle
+
+    area=length*width
+
+"""
+
+length=int(input("Enter length: "))
+width=int(input("Enter width: "))
+area=length*width
+print("The area of the rectangle is",area)
